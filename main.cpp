c5358b5088ccb8ff6aac9071a63a9f842862a231
ealkaabi1 <ealkaabi1@mail.csuchico.edu>
2017-11-06 09:33:53 -0800

    Steps in comments

---
+++ main.cpp
@@ -0,0 +1,39 @@
+// Eisa Alkaabi
+// Project 3
+
+#include "bitmap.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+  // repeat
+    // ask user for filename until they enter 10 filenames or the input is 
+    // "DONE"
+    //
+    //
+    // if a filename has problems then print them error msg
+    //   -print if the file does not exist
+    //   -print if the file is not valid bitmap
+    //   -print of the file is not the same width and height dimension as
+    //    the first image loaded
+    //
+    // continue with other files but disregard the problem files
+    //
+    // after all of the files are read in,
+    //
+    // if the user did not give at least 2 images with width and height
+    //   -print error message
+    //   -do nothing else
+    //
+    // else combine the images
+    //
+    // loop through all of the images by the pixels
+    //    -make each pixel average of RGB values of all the pixels
+    //    -print progress updates in middle of loop to screen
+    //
+    // save file as composite-ealkaabi1.bmp
+    // exit
+  return 0;
+}
