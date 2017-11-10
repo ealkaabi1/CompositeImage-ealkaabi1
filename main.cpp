7df3abd94a48851bb837300681657ef920b34400
ealkaabi1 <ealkaabi1@mail.csuchico.edu>
2017-11-10 15:11:22 -0800

    Added helper functions to make code more readable

--- main.cpp
+++ main.cpp
@@ -12,13 +12,19 @@ void fill_bitmap_vector(vector<Bitmap>& bmps);
 //do the logic to make the composite in this function
 Bitmap make_composite(Bitmap bmp);
 
+// helper function to check if the filename pass in is a valid bmp file
+bool is_good_bmp(string filename);
+
+// helper function to check two bitmap have the same dimension
+bool equal_dimensions(Bitmap first, Bitmap second);
+
 int main()
 {
   vector<Bitmap> bmps;
   Bitmap composite;
   
   fill_bitmap_vector(bmps);
-  bmps = make_composite(bmps);
+  //bmps = make_composite(bmps);
 
   // save the finish product!
   composite.save("composite-ealkaabi1.bmp");
@@ -35,23 +41,18 @@ void fill_bitmap_vector(vector<Bitmap>& bmps)
   // read in the filenames
   while (count < 10 && filename != "DONE")
   {
+    Bitmap temp;
+
     cout << "Filename #" << count+1 <<": ";
     getline(cin, filename);
 
-    if (filename != "DONE")
+    if (filename != "DONE" && is_good_bmp(filename))
     {
-
-      Bitmap temp;
-      temp.open(filename);
-
-      if (!temp.isImage());
-      else if (bmps.size() > 0)
+      if (bmps.size() > 0)
       {
-        PixelMatrix first_matrix = bmps[0].toPixelMatrix();
-        PixelMatrix temp_matrix = temp.toPixelMatrix();
+        temp.open(filename);
 
-        if (temp_matrix.size() != first_matrix.size()
-            || temp_matrix[0].size() != first_matrix[0].size())
+        if (!equal_dimensions(bmps[0], temp))
           cout << "\"" << filename << "\" does not have the same dimensions"
             << " as the first image loaded." << endl;
         else
@@ -73,4 +74,29 @@ Bitmap make_composite(Bitmap bmp)
 {
   Bitmap implement;
   return implement;
+}
+bool is_good_bmp(string filename)
+{
+  Bitmap temp;
+  temp.open(filename);
+
+  if (!temp.isImage())
+    return false;
+  
+  return true;
+}
+
+bool equal_dimensions(Bitmap first, Bitmap second)
+{
+  PixelMatrix first_matrix = first.toPixelMatrix();
+  PixelMatrix second_matrix = second.toPixelMatrix();
+        
+  if (first_matrix.size() != second_matrix.size())
+    return false;
+  else
+    for (int i = 0; i < first_matrix.size(); i++)
+      if (first_matrix[i].size() != second_matrix[i].size())
+        return false;
+
+  return true;
 }
